41f2b47aaa2737c14914b735d14069e65bb63097
Morgan Sorbaro <msorbaro@gmail.com>
2019-10-23 19:21:38 -0400

    adding my canged

--- App.js
+++ App.js
@@ -1,11 +1,31 @@
 import React from 'react';
+import axios from 'axios';
+
 import { StyleSheet, Text, View } from 'react-native';
+
+const ROOT_URL = 'http://localhost:9090/';
+
+function functionCall() {
+  axios.get(`${ROOT_URL}`).then((response) => {
+    const data = response.data.result;
+    console.log("responSE");
+    return data;
+  }).catch((error) => {
+      // hit an error do something else!
+      console.log(error.response.data);
+    });
+}
+
+datafromcall = functionCall();
+datafromcall = functionCall();
+
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hello World!!1 YAY FTG</Text>
       <Text>morgan was here</Text>
+      <Text>{datafromcall}</Text>
     </View>
   );
 }
