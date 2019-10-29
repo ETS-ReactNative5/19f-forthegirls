a825fa9c3de92e7d62fcf772421f69477bd296c5
Annika Kouhia <annika.j.kouhia.20@dartmouth.edu>
2019-10-29 18:14:45 -0400

    explore page with hardcode values

--- Components/Events.js
+++ Components/Events.js
@@ -1,14 +1,42 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import ExploreEvent from './ExploreEvent.js'
 
 class Events extends React.Component {
   constructor(props) {
     super(props);
+
+    this.renderEvent = this.renderEvent.bind(this);
+    this.renderEvents = this.renderEvents.bind(this);
+  }
+
+  renderEvent (nameProp) {
+    return (
+        <View>
+          <ExploreEvent name={nameProp} />
+        </View>
+    );
+  }
+
+  renderEvents(){
+    var eventList = ['Speech', 'Mixer', 'Hackathon'];
+
+    var renderedEvents = eventList.map((anEvent) => {
+        return (
+          this.renderEvent(anEvent)
+        );
+      }
+    )
+
+    return renderedEvents;
   }
 
   render() {
+    //return this.renderEvent('Speech');
     return (
-      <Text>I am a event </Text>
+      <ScrollView>
+        {this.renderEvents()}
+      </ScrollView>
     );
   }
 }
