5627f5a4e54ae30233f4a92c82c053e00ffb51b5
Sami BUrack <samiburack@gmail.com>
2019-11-12 18:18:55 -0500

    chat styling

--- Components/Chats.js
+++ Components/Chats.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import { getUser, getMatch } from '../actions';
 import chatList from '../assets/styles/chatStyle';
-import { fonts } from '../assets/styles/basicStyle';
 import { Linking } from 'react-native'
-
+import colors, { fonts, fontEffects, buttons } from '../assets/styles/basicStyle';
 
 class Chats extends React.Component {
   constructor(props) {
@@ -24,29 +23,36 @@ class Chats extends React.Component {
   }
 
   pressUser = (email) => {
-    Linking.openURL('mailto:'+email+'?subject=We Matched!')
-    .catch((error) => console.log(error));
+    Linking.openURL('mailto:' + email + '?subject=We Matched!')
+      .catch((error) => console.log(error));
   }
 
   showMatches() {
-      // const matchInfo = getMatch(this.props.matches[i].id);
-      // console.log("MATCH INFO");
-      // console.log(matchInfo);
-
-      return this.props.matches.map((n) => {
-        return (
-          // [chatList.listItem, i % 2 === 0 ? 
-        <View key={n._id} style={chatList.listItemPurple}>
-          <Text style={fonts.bodyText} key={n.username}>{n.username}!</Text>
-          <Button key={n._id} title="Chat!" onPress={() => this.pressUser(n.email)} /> 
+    // const matchInfo = getMatch(this.props.matches[i].id);
+    // console.log("MATCH INFO");
+    // console.log(matchInfo);
+    var i = -1;
+    return this.props.matches.map((n) => {
+      i++;
+      return (
+        <View key={n._id} style={[i % 2 === 0 ? chatList.listItemPurple : chatList.listItemWhite, chatList.listItem]}>
+          <Text style={[fonts.minorHeading, chatList.username]} key={n.username}>{n.username}</Text>
+          <View style={chatList.chatButton}>
+            <TouchableOpacity
+              key={n._id}
+              onPress={() => this.pressUser(n.email)}>
+              <Text style={[fonts.majorHeading, colors.turquoise, fontEffects.center]}>Chat</Text>
+            </TouchableOpacity>
+          </View>
         </View>
-        );
-      });
+      );
+    });
   }
 
   render() {
     return (
-      <View style={chatList.chatContainer}>
+      <View>
+        <Text style={[colors.deepPurple, fonts.majorHeading, fontEffects.center]}>Matches</Text>
         {this.showMatches()}
       </View>
     );
