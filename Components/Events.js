de831d493f9e24581f5e8cc9997b1a604b07f936
Annika Kouhia <annika.j.kouhia.20@dartmouth.edu>
2019-11-13 19:06:45 -0500

    fetching events from backend successfully

--- Components/Events.js
+++ Components/Events.js
@@ -3,6 +3,8 @@ import { StyleSheet, Text, View, ScrollView, Button } from 'react-native';
 import SingleEvent from './SingleEvent.js'
 import mainScreenStyle from '../assets/styles/mainStyle';
 import eventPage from '../assets/styles/eventPage';
+import { connect } from 'react-redux';
+import { fetchEvents } from '../actions';
 
 class Events extends React.Component {
   static navigationOptions = {
@@ -15,6 +17,10 @@ class Events extends React.Component {
     this.renderEvent = this.renderEvent.bind(this);
     this.renderEvents = this.renderEvents.bind(this);
     this.navToAdd = this.navToAdd.bind(this);
+  }
+
+  componentDidMount() {
+    this.props.fetchEvents();
   }
 
   navToAdd() {
@@ -31,10 +37,12 @@ class Events extends React.Component {
 
   renderEvents() {
     var eventList = ['Speech', 'Mixer', 'Hackathon'];
+    console.log(this.props.events.all);
 
-    var renderedEvents = eventList.map((anEvent) => {
+    var renderedEvents = this.props.events.all.map((anEvent) => {
+      console.log('an event ' + anEvent);
       return (
-        this.renderEvent(anEvent, anEvent.toString())
+        this.renderEvent(anEvent.title, anEvent._id)
       );
     })
 
@@ -53,4 +61,11 @@ class Events extends React.Component {
   }
 }
 
-export default Events;
+// connects particular parts of redux state to this components props
+const mapStateToProps = state => (
+  {
+    events: state.events,
+  }
+);
+
+export default (connect(mapStateToProps, { fetchEvents })(Events));
--- reducers/event_reducer.js
+++ reducers/event_reducer.js
@@ -1,24 +1,24 @@
 import { ActionTypes } from '../actions';
 
 const EventReducer = (state = {
-    rsvps: [],
-    title: '',
-    date: '',
-    time: '',
-    location: '',
-    description: '',
+    all: [{}],
+    event: {},
   }, action) => {
     switch (action.type) {
-      case ActionTypes.ADD_EVENT:
-      console.log('made it to reducer');
-        return Object.assign({}, state, {
-          rsvps: action.apyload.result.rsvps,
-          title: action.payload.result.title,
-          date: action.payload.result.date,
-          time: action.payload.result.time,
-          location: action.payload.result.location,
-          description: action.payload.result.description,
-        });
+      // case ActionTypes.ADD_EVENT:
+      // console.log('made it to reducer');
+      //   return Object.assign({}, state, {
+      //     rsvps: action.apyload.result.rsvps,
+      //     title: action.payload.result.title,
+      //     date: action.payload.result.date,
+      //     time: action.payload.result.time,
+      //     location: action.payload.result.location,
+      //     description: action.payload.result.description,
+      //   });
+      case ActionTypes.FETCH_EVENTS:
+        return Object.assign({}, state, { all: action.payload });
+      case ActionTypes.FETCH_EVENT:
+        return Object.assign({}, state, { event: action.payload });
       default:
         return state;
     }
