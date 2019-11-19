d63e1fa2805a2b28306fc3353e0027717ea1d04c
Sami BUrack <samiburack@gmail.com>
2019-11-19 17:57:41 -0500

    between

--- Components/Signup.js
+++ Components/Signup.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button, TouchableOpacity,  Image } from 'react-native';
+import { StyleSheet, Text, View, Button, TouchableOpacity, Image } from 'react-native';
 import { Dropdown } from 'react-native-material-dropdown';
 import TextField from 'react-native-text-field';
 import colors, { fonts, fontEffects } from '../assets/styles/basicStyle';
@@ -29,22 +29,22 @@ class SignUp extends React.Component {
     }];
 
     return (
-      <View style={{backgroundColor: '#46518725', width: '100%', height: '100%', justifyContent: 'center'}}>
-        <View style={{backgroundColor: '#FFFFFF', alignItems: 'center'}}>
-          <Text style={[fonts.majorHeading, {alignItems: 'center'}]}>Hi! Welcome to <Text style={{color:'#28C3A9'}}> For the Girls </Text> </Text>
+      <View style={{ backgroundColor: '#46518725', width: '100%', height: '100%', justifyContent: 'center' }}>
+        <View style={{ backgroundColor: '#FFFFFF', alignItems: 'center' }}>
+          <Text style={[fonts.majorHeading, { alignItems: 'center' }]}>Hi! Welcome to <Text style={{ color: '#28C3A9' }}> For the Girls </Text> </Text>
         </View>
-        <View style={{backgroundColor: '#28C3A975', alignItems: 'center', paddingTop: 5, paddingBottom: 5}}>
-          <Text style={[fonts.minorHeading, {textAlign: 'center'}]}> We are a mentorship app committed to creating lasting mentorship relationships bebtween fellow women in tech </Text>
-          <Text  style={[fonts.minorHeading, {color:'#FFFFFF', marginTop:  '5%'}]}> created by women, for women </Text>
+        <View style={{ backgroundColor: '#28C3A975', alignItems: 'center', paddingTop: 5, paddingBottom: 5 }}>
+          <Text style={[fonts.minorHeading, { textAlign: 'center' }]}> We are a mentorship app committed to creating lasting mentorship relationships between fellow women in tech </Text>
+          <Text style={[fonts.minorHeading, { color: '#FFFFFF', marginTop: '5%' }]}> created by women, for women </Text>
         </View>
 
-        <View style={{width:'100%', flexDirection: 'row-reverse', alignItems: 'right', marginTop: 30}}>
+        <View style={{ width: '100%', flexDirection: 'row-reverse', alignItems: 'right', marginTop: 30 }}>
           <TouchableOpacity
-            onPress={() => {  this.props.navigation.navigate('BasicInfo', {}) }}
-            style={{marginRight: 20}}>
+            onPress={() => { this.props.navigation.navigate('BasicInfo', {}) }}
+            style={{ marginRight: 20 }}>
             <Image
               source={require('./../assets/icons/arrownext.png')}
-              />
+            />
           </TouchableOpacity>
         </View>
       </View>
