95e9a8ae18dda828ad0b2d110c7e33701b160a3f
Sami BUrack <samiburack@gmail.com>
2020-01-09 18:12:21 -0500

    show hide skills + start on dropdown modularization

--- Components/csComponent.js
+++ Components/csComponent.js
@@ -31,6 +31,11 @@ class CsComponent extends React.Component {
       security: false,
       algorithms: false,
       storage: false,
+
+      // showing and hiding 
+      showSkills: false,
+      showPreferences: false,
+      showCompany: false
     }
 
     this.handleFieldChange = this.handleFieldChange.bind(this);
@@ -42,6 +47,62 @@ class CsComponent extends React.Component {
 
   submitPage = () => {
     this.props.addToSurvey(this.state, this.props.username, this.props.navigation, 'Prompts');
+  }
+
+  showSkills() {
+    this.setState({
+      showSkills: !this.showSkills
+    })
+    if (this.state.showSkills) {
+      return (
+        <View style={surveyStyle.items}>
+          <TouchableComponent name='Web Applications' stateField='web' stateFieldStatus={this.state.web} onChange={this.handleFieldChange} />
+          <TouchableComponent name='User Interaction' stateField='user' stateFieldStatus={this.state.user} onChange={this.handleFieldChange} />
+          <TouchableComponent name='Design' stateField='design' stateFieldStatus={this.state.design} onChange={this.handleFieldChange} />
+          <TouchableComponent name='Mobile Applications' stateField='mobile' stateFieldStatus={this.state.mobile} onChange={this.handleFieldChange} />
+          <TouchableComponent name='Security' stateField='security' stateFieldStatus={this.state.security} onChange={this.handleFieldChange} />
+          <TouchableComponent name='Algorithms & Math' stateField='algorithms' stateFieldStatus={this.state.algorithms} onChange={this.handleFieldChange} />
+          <TouchableComponent name='Storage & Infrastructure' stateField='storage' stateFieldStatus={this.state.storage} onChange={this.handleFieldChange} />
+        </View>
+      )
+    }
+  }
+
+  showPreferences() {
+    this.setState({
+      showPreferences: !this.showPreferences
+    })
+    if (this.state.showPreferences) {
+      return (
+        <View style={surveyStyle.items}>
+          <TouchableComponent name='Front End' stateField='frontEnd' stateFieldStatus={this.state.frontEnd} onChange={this.handleFieldChange} />
+          <TouchableComponent name='Back End' stateField='backEnd' stateFieldStatus={this.state.backEnd} onChange={this.handleFieldChange} />
+        </View>
+      );
+    }
+  }
+
+  showCompany() {
+    this.setState({
+      showCompany: !this.showCompany
+    })
+    if (this.state.showCompany) {
+      return (
+        <View style={surveyStyle.items}>
+          <TouchableComponent name='Small' stateField='small' stateFieldStatus={this.state.small} onChange={this.handleFieldChange} />
+          <TouchableComponent name='Medium' stateField='medium' stateFieldStatus={this.state.medium} onChange={this.handleFieldChange} />
+          <TouchableComponent name='Large' stateField='large' stateFieldStatus={this.state.large} onChange={this.handleFieldChange} />
+          <TouchableComponent name='Meritocratic' stateField='meritocratic' stateFieldStatus={this.state.meritocratic} onChange={this.handleFieldChange} />
+          <TouchableComponent name='Nurturing' stateField='nurturing' stateFieldStatus={this.state.nurturing} onChange={this.handleFieldChange} />
+          <TouchableComponent name='Fratty' stateField='fratty' stateFieldStatus={this.state.fratty} onChange={this.handleFieldChange} />
+          <TouchableComponent name='Fast-Paced' stateField='fast' stateFieldStatus={this.state.fast} onChange={this.handleFieldChange} />
+          <TouchableComponent name='Organized' stateField='organized' stateFieldStatus={this.state.organized} onChange={this.handleFieldChange} />
+          <TouchableComponent name='Stable' stateField='stable' stateFieldStatus={this.state.stable} onChange={this.handleFieldChange} />
+          <TouchableComponent name='Formal' stateField='formal' stateFieldStatus={this.state.formal} onChange={this.handleFieldChange} />
+          <TouchableComponent name='Relaxed' stateField='relaxed' stateFieldStatus={this.state.relaxed} onChange={this.handleFieldChange} />
+        </View>
+      )
+    }
   }
 
   render() {
@@ -75,45 +136,26 @@ class CsComponent extends React.Component {
         <View style={{ alignItems: 'center', width: '100%', marginTop: 10, marginBottom: 10 }}>
           <SurveyHeaderComponent text="Now, tell us what your interests are in computer science" header="CompSci Interests" />
         </View>
-        <View>
-          <Text style={headerText}>Front End or Back End?</Text>
-          <View style={surveyStyle.items}>
-            <TouchableComponent name='Front End' stateField='frontEnd' stateFieldStatus={this.state.frontEnd} onChange={this.handleFieldChange} />
-            <TouchableComponent name='Back End' stateField='backEnd' stateFieldStatus={this.state.backEnd} onChange={this.handleFieldChange} />
-          </View>
-        </View>
-        <View>
-          <Text style={headerText}>Dream Company Size?</Text>
-          <View style={surveyStyle.items}>
-            <TouchableComponent name='Small' stateField='small' stateFieldStatus={this.state.small} onChange={this.handleFieldChange} />
-            <TouchableComponent name='Medium' stateField='medium' stateFieldStatus={this.state.medium} onChange={this.handleFieldChange} />
-            <TouchableComponent name='Large' stateField='large' stateFieldStatus={this.state.large} onChange={this.handleFieldChange} />
-          </View>
-        </View>
-        <View>
-          <Text style={headerText}>Preferred Work Culture?</Text>
-          <View style={surveyStyle.items}>
-            <TouchableComponent name='Meritocratic' stateField='meritocratic' stateFieldStatus={this.state.meritocratic} onChange={this.handleFieldChange} />
-            <TouchableComponent name='Nurturing' stateField='nurturing' stateFieldStatus={this.state.nurturing} onChange={this.handleFieldChange} />
-            <TouchableComponent name='Fratty' stateField='fratty' stateFieldStatus={this.state.fratty} onChange={this.handleFieldChange} />
-            <TouchableComponent name='Fast-Paced' stateField='fast' stateFieldStatus={this.state.fast} onChange={this.handleFieldChange} />
-            <TouchableComponent name='Organized' stateField='organized' stateFieldStatus={this.state.organized} onChange={this.handleFieldChange} />
-            <TouchableComponent name='Stable' stateField='stable' stateFieldStatus={this.state.stable} onChange={this.handleFieldChange} />
-            <TouchableComponent name='Formal' stateField='formal' stateFieldStatus={this.state.formal} onChange={this.handleFieldChange} />
-            <TouchableComponent name='Relaxed' stateField='relaxed' stateFieldStatus={this.state.relaxed} onChange={this.handleFieldChange} />
-          </View>
-        </View>
+
         <View>
           <Text style={headerText}>CS Strengths?</Text>
-          <View style={surveyStyle.items}>
-            <TouchableComponent name='Web Applications' stateField='web' stateFieldStatus={this.state.web} onChange={this.handleFieldChange} />
-            <TouchableComponent name='User Interaction' stateField='user' stateFieldStatus={this.state.user} onChange={this.handleFieldChange} />
-            <TouchableComponent name='Design' stateField='design' stateFieldStatus={this.state.design} onChange={this.handleFieldChange} />
-            <TouchableComponent name='Mobile Applications' stateField='mobile' stateFieldStatus={this.state.mobile} onChange={this.handleFieldChange} />
-            <TouchableComponent name='Security' stateField='security' stateFieldStatus={this.state.security} onChange={this.handleFieldChange} />
-            <TouchableComponent name='Algorithms & Math' stateField='algorithms' stateFieldStatus={this.state.algorithms} onChange={this.handleFieldChange} />
-            <TouchableComponent name='Storage & Infrastructure' stateField='storage' stateFieldStatus={this.state.storage} onChange={this.handleFieldChange} />
-          </View>
+          <TouchableOpacity
+            onPress={this.showSkills}>
+          </TouchableOpacity>
+        </View>
+
+        <View>
+          <Text style={headerText}>CS Preferences</Text>
+          <TouchableOpacity
+            onPress={this.showPrefs}>
+          </TouchableOpacity>
+        </View>
+
+        <View>
+          <Text style={headerText}>Company Preferences</Text>
+          <TouchableOpacity
+            onPress={this.showCompany}>
+          </TouchableOpacity>
         </View>
 
         <View style={buttons.arrowView}>
