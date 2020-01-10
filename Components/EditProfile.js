1c8eba776fd65e3230ec844eaeca488b8a52835c
Morgan Sorbaro <msorbaro@gmail.com>
2020-01-10 15:07:24 -0500

    adding in questions ad slides to edit profile page

--- Components/EditProfile.js
+++ Components/EditProfile.js
@@ -13,20 +13,132 @@ import { connect } from 'react-redux';
 class EditProfile extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {   };
+    this.state = {
+      promptOneQuestion: '',
+      promptOneAnswer: '',
+      promptTwoQuestion: '',
+      promptTwoAnswer: '',
+      promptThreeQuestion: '',
+      promptThreeAnswer: '',
+      introextro: 50,
+      listenfollow: 50
+    };
+    this.handleSliderChange = this.handleSliderChange.bind(this);
+
   }
+
+  p1Question = (value) => {
+    this.setState({ promptOneQuestion: value });
+  }
+
+  p1Answer = (text) => {
+    this.setState({ promptOneAnswer: text });
+  }
+
+  p2Question = (value) => {
+    this.setState({ promptTwoQuestion: value });
+  }
+
+  p2Answer = (text) => {
+    this.setState({ promptTwoAnswer: text });
+  }
+
+  p3Question = (value) => {
+    this.setState({ promptThreeQuestion: value });
+  }
+
+  p3Answer = (text) => {
+    this.setState({ promptThreeAnswer: text });
+  }
+
+  handleSliderChange(sliderId, value) {
+    this.setState({ [sliderId]: value });
+    // console.log(`parent: ${this.state.introextro}`);
+  }
+
   opacityOnPress = () => {
     this.props.navigation.navigate('Home', { })
   }
+
   render() {
+    let data = [{
+      value: 'Woman in tech inspiration?',
+    }, {
+      value: 'Favorite app?',
+    }, {
+      value: 'iOS or android?',
+    }, {
+      value: 'favorite programming language',
+    }, {
+      value: 'coffee or tea',
+    }, {
+      value: 'spaces or tabs',
+    }, {
+      value: 'what could you give a ted talk on',
+    }];
+
+
+    var textFieldStyle = [surveyStyle.textField, fonts.bodyText]
+    var itemTextStyle = [fonts.bodyText]
+    var selectedItemColor = colors.turquoise.color
+
     return (
-     <View>
-      <Text> edit me :) </Text>
-      <TouchableOpacity
-          onPress={this.opacityOnPress}>
-          <Text>go back</Text>
-      </TouchableOpacity>
-     </View>
+
+      <ScrollView style={surveyStyle.surveyBackground}>
+        <View style={{ alignItems: 'center', width: '100%', marginTop: 10, marginBottom: 10 }}>
+          <SurveyHeaderComponent text="Tell us about your personality" header="How Chill Are You?" />
+        </View>
+        <Dropdown
+          itemTextStyle={itemTextStyle}
+          selectedItemColor={selectedItemColor}
+          label='Question 1'
+          data={data}
+          onChangeText={this.p1Question}
+        />
+        <TextInput
+          style={textFieldStyle}
+          placeholder="Prompt 1 Answer"
+          onChangeText={this.p1Answer}
+        />
+        <Dropdown
+          itemTextStyle={itemTextStyle}
+          selectedItemColor={selectedItemColor}
+          label='Question 2'
+          data={data}
+          onChangeText={this.p2Question}
+        />
+        <TextInput
+          style={textFieldStyle}
+          placeholder="Prompt 2 Answer"
+          onChangeText={this.p2Answer}
+        />
+        <Dropdown
+          itemTextStyle={itemTextStyle}
+          selectedItemColor={selectedItemColor}
+          label='Question 3'
+          data={data}
+          onChangeText={this.p3Question}
+        />
+        <TextInput
+          style={textFieldStyle}
+          placeholder="Prompt 3 Answer"
+          onChangeText={this.p3Answer}
+        />
+
+        <View style={{ alignItems: 'center', width: '100%', marginTop: 10, marginBottom: 10 }}>
+          <SurveyHeaderComponent header="Personality Sliders" text="help us match you better!" />
+        </View>
+        <SliderComponent id='introextro' onChange={this.handleSliderChange} value={this.state.introextro} min='introvert' max='extrovert' />
+        <SliderComponent id='listenfollow' onChange={this.handleSliderChange} value={this.state.listenfollow} min='listener' max='leader' />
+
+        <TouchableOpacity
+            onPress={this.opacityOnPress}>
+            <Text>go back</Text>
+        </TouchableOpacity>
+      </ScrollView>
+
+
+
     );
   }
 }
