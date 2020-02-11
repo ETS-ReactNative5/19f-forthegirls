78c7d4d55aa30ae9fd49c470b7ec56f5546f3e6b
Annika Kouhia <annika.j.kouhia.20@dartmouth.edu>
2020-02-11 17:50:44 -0500

    activity tracks on front end

--- navigation/Main.js
+++ navigation/Main.js
@@ -1,33 +1,39 @@
 import React, { Component, useEffect } from 'react';
-
-// import { StyleSheet, Text, View } from 'react-native';
 import MainTabBar from './MainTabBar';
 import SignInUpStack from './signInUpStack';
+import { addActivity } from '../actions';
 import { connect } from 'react-redux';
-
-
 
 class Main extends Component {
   constructor(props) {
     super(props);
-  
+
+  }
+
+  componentDidMount() {
+    if(this.props.auth.id){
+      this.props.addActivity({
+        id: this.props.auth.id,
+        timestamp: Date.now(),
+      });
+    }
   }
 
 
   render() {
-    if(this.props.auth) {
+    if(this.props.auth.authenticated) {
       return <MainTabBar/>
     }
     else {
       return <SignInUpStack/>
-    } 
+    }
   }
 }
 
 const mapStateToProps = reduxState => (
   {
-    auth: reduxState.auth.authenticated,
+    auth: reduxState.auth,
   }
 );
 
-export default connect(mapStateToProps, null)(Main);
+export default connect(mapStateToProps, { addActivity })(Main);
