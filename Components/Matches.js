71146bf3695e04e73abddb4d0f3d69e3c11d46f4
Sami BUrack <samiburack@gmail.com>
2020-02-13 21:42:51 -0500

    loading screen start

--- Components/Matches.js
+++ Components/Matches.js
@@ -1,34 +1,63 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  Animated,
+  Image,
+  Easing
+} from 'react-native';
 import PotentialMentor from './PotentialMentor';
 import { getPotentialMatches } from '../actions';
 
 class Matches extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      animation: false
+    }
+    this.spinValue = new Animated.Value(0);
 
     this.returnMatches = this.returnMatches.bind(this);
   }
 
   componentDidMount() {
     this.props.getPotentialMatches(this.props.username);
+    this.spin();
   }
 
   refresh = () => {
     this.props.getPotentialMatches(this.props.username);
   }
 
+  spin() {
+    this.spinValue.setValue(0)
+    Animated.timing(
+      this.spinValue,
+      {
+        toValue: 1,
+        duration: 500,
+        easing: Easing.inOut(Easing.ease)
+      }
+    ).start(() => this.spin())
+  }
+
   returnMatches = () => {
     return this.props.potentialMatches.map((n) => {
       return (
-        <PotentialMentor key={n} userId={n} refresh={this.refresh} navigation={this.props.navigation}/>
+        <PotentialMentor key={n} userId={n} refresh={this.refresh} navigation={this.props.navigation} />
       )
     });
   }
 
   render() {
-    if (this.props.potentialMatches !== undefined && this.props.potentialMatches.legnth !== 0) {
+    const spin = this.spinValue.interpolate({
+      inputRange: [0, 1],
+      outputRange: ['0deg', '360deg']
+    })
+    if (this.props.potentialMatches !== undefined && this.props.potentialMatches.legnth !== 0 && this.state.animation === false) {
       return (
         <ScrollView>
           {this.returnMatches()}
@@ -42,7 +71,18 @@ class Matches extends React.Component {
     }
     else {
       return (
-        <Text>Loading...</Text>
+        <View style={styles.container}>
+          <Animated.Image
+            style={{
+              justifyContent: 'center',
+              alignItems: 'center',
+              height: 500,
+              width: 500,
+              transform: [{ rotate: spin }]
+            }}
+            source={require('./../assets/icons/loading.png')}
+          />
+        </View>
       )
     }
 
