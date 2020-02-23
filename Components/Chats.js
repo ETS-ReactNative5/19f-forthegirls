1c6cd36fe42e430f0f0a461a06643c59c90a1a20
Alexis Harris <alexisbharris@gmail.com>
2020-02-23 15:23:09 -0500

    initial scaling and loading screen for chats

--- Components/Chats.js
+++ Components/Chats.js
@@ -1,22 +1,53 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { StyleSheet, Text, View, TouchableOpacity, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, ScrollView, Animated, Easing } from 'react-native';
 import { getUser, getMatches, deleteMatch } from '../actions';
 import colors, { fonts, fontEffects, buttons } from '../assets/styles/basicStyle';
 import axios from 'axios';
 import Match from './Match';
 
+
+/* animation: 
+ // https://facebook.github.io/react-native/docs/animated#timing
+ // https://stackoverflow.com/questions/37445090/react-native-how-do-you-animate-the-rotation-of-an-image
+*/
+
 class Chats extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      animation: false,
+    }
+
+    this.spinValue = new Animated.Value(0);
   }
 
   componentDidMount() {
     this.props.getUser(this.props.id);
+    this.spin();
+    this.setState({ animation: true });
   }
 
   deleteMatch = (matchID) => {
     this.props.deleteMatch(this.props.id, matchID, this.props.username);
+  }
+
+  spin() {
+    this.spinValue.setValue(0)
+    Animated.timing(
+      this.spinValue,
+      {
+        toValue: 1,
+        duration: 500,
+        easing: Easing.inOut(Easing.ease),
+        useNativeDriver: true
+      }
+    ).start(() => this.spin())
+    if ((this.props.potentialMatches !== undefined && this.props.potentialMatches.legnth !== 0) ||
+      (this.props.potentialMatches !== undefined && this.props.potentialMatches.legnth === 0)) {
+      this.setState({ animation: false })
+    }
+
   }
 
   showMatches() {
@@ -37,6 +68,28 @@ class Chats extends React.Component {
   }
 
   render() {
+
+    const spin = this.spinValue.interpolate({
+      inputRange: [0, 1],
+      outputRange: ['0deg', '360deg']
+    })
+    var loading = (
+      <View style={{
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100%'
+      }}>
+        <Animated.Image
+          style={{
+            height: 200,
+            width: 200,
+            transform: [{ rotate: spin }]
+          }}
+          source={require('./../assets/icons/loading.png')}
+        />
+        <Text style={[fonts.majorHeading, colors.deepPurple]}>Loading Your Chats!</Text>
+      </View>);
+
     if (this.props.matches !== undefined && this.props.matches.legnth !== 0) {
       return (
         <ScrollView>
@@ -57,9 +110,7 @@ class Chats extends React.Component {
     }
     else {
       this.props.getMatches(this.props.username);
-      return (
-        <Text>Loading...</Text>
-      )
+      return (loading);
     }
 
   }
